2fe6858ca1e0427057b99a5be11f21195d0eb006
Neury A Frias <neury0102@gmail.com>
2016-10-02 13:15:44 -0400

    conexion a la base de datos creacion de hibernate

--- src/main/java/com/example/Practica10Application.java
+++ src/main/java/com/example/Practica10Application.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class Practica10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Practica10Application.class, args);
+
 	}
 }
---
+++ src/main/java/com/example/entidades/Usuario.java
@@ -0,0 +1,55 @@
+package com.example.entidades;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by vacax on 20/09/16.
+ */
+@Entity
+public class Usuario implements Serializable{
+
+    @Id
+    private String username = "";
+    private String nombre;
+    private String apellido = "";
+    private String password= "";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+}
